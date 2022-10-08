d9172c3a66329c680fa0583c5f677c1dcc5b954d
Natalia CM <mnacamu@gmail.com>
2022-10-08 21:00:41 +0200

    Añadidas paginas home, people, tasks y assign. Y menu lateral.

--- src/app/pages/people/people.page.ts
+++ src/app/pages/people/people.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HomePage } from '../home/home.page';
 
 @Component({
   selector: 'app-people',
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title>PEOPLE</ion-title>
+      </ion-toolbar>
+    </ion-header>
+  `,
   templateUrl: './people.page.html',
   styleUrls: ['./people.page.scss'],
 })
-export class PeoplePage implements OnInit {
 
-  constructor() { }
+export class PeoplePage {
 
-  ngOnInit() {
-  }
+  component = HomePage;
 
 }
